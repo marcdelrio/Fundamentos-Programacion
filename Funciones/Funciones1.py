1f4804ee3e02cd69bc428e7e267df7b9cb6d071b
marcdelrio <marc.delrio@duocuc.cl>
2025-06-10 19:54:15 -0400

    Update Funciones1.py

--- Funciones/Funciones1.py
+++ Funciones/Funciones1.py
@@ -8,6 +8,8 @@ while estado:
         valor2 = int(input(f"Ingrese el segundo valor: "))
         resultado = suma(valor1,valor2)
         print(f"La suma es {resultado}")
+
+        print(f"La suma sin variable de retorno es {suma(valor1, valor2)}")
         estado = False
     except ValueError:
         print("Error: Dato incorrectos.")
